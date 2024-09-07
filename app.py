6908d7c419b666cd99e7d47e11ab50fe72276514
Mihir Thakur <tamihir27@yahoo.com>
2024-09-06 19:49:39 -0700

    Update IO Messaging

--- app.py
+++ app.py
@@ -15,22 +15,29 @@ def chat():
 @socketio.on('message')
 def handle_message(data):
     user_message = data['message']
-    print(user_message)
+    print(user_message, type(user_message))
     
     # Process the user message
-    # response = generate_chatbot_response(user_message)
-    response = "Test"
+    response = generate_chatbot_response(user_message)
+    print(response)
+    # response = "Test"
     
     # Send the response back to the user
     emit('response', {'message': response})
 
 def generate_chatbot_response(message):
-    if "search" in message.lower():
-        query_embedding = rag.generate_embedding(message)
-        results = rag.search_docs(by="embedding", query=query_embedding)
-        return f"Found {len(results)} results for your search."
-    else:
-        return "I'm here to help! You can ask me to search for PDFs."
+    # if "search" in message.lower():
+    # query_embedding, shape = rag.generate_embeddings(message)
+    # print(query_embedding)
+    docs = rag.search_docs(by="embedding", query=message)
+    # print(results[0]['text'])
+    text = [x[2:] for x in docs[0]['text']]
+    text = ''.join(text)
+    res = rag.generate_query_output(query=message, context=text)
+    # print(res)
+    return res
+    # else:
+    #     return "I'm here to help! You can ask me to search for PDFs."
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
